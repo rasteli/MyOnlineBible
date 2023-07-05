5e6294eb2523ae56a8267cd0646a0ed4141b203e
Gabriel Rasteli <gabrielrasteli3@gmail.com>
2023-07-05 12:15:13 -0300

    fix: Replaced Job chapter abbreviation

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,11 +1,11 @@
-import clsx from "clsx"
-import { useState, useRef, useEffect } from "react"
-import { List, X } from "phosphor-react"
-import autoAnimate from "@formkit/auto-animate"
+import clsx from 'clsx'
+import { useState, useRef, useEffect } from 'react'
+import { List, X } from 'phosphor-react'
+import autoAnimate from '@formkit/auto-animate'
 
-import { Collapsible } from "./Collapsible"
-import { useBook } from "../../contexts/BookContext"
-import { useViewport } from "../../hooks/useViewport"
+import { Collapsible } from './Collapsible'
+import { useBook } from '../../contexts/BookContext'
+import { useViewport } from '../../hooks/useViewport'
 
 export function Sidebar() {
   const parent = useRef(null)
@@ -22,8 +22,8 @@ export function Sidebar() {
     }
   }, [parent])
 
-  const vt_books = books.filter(book => book.testament === "VT")
-  const nt_books = books.filter(book => book.testament === "NT")
+  const vt_books = books.filter(book => book.testament === 'VT')
+  const nt_books = books.filter(book => book.testament === 'NT')
 
   function toggle() {
     setOpen(!open)
@@ -36,20 +36,20 @@ export function Sidebar() {
   return (
     <aside
       ref={parent}
-      className={clsx("bg-gray-100 px-5 pt-5 shadow-mob sticky top-0", {
-        "rounded-tr-xl rounded-br-xl": aboveThreshold,
-        "h-20": !aboveThreshold && !menuOpen,
-        "h-screen": aboveThreshold || menuOpen
+      className={clsx('bg-gray-100 px-5 pt-5 shadow-mob sticky top-0', {
+        'rounded-tr-xl rounded-br-xl': aboveThreshold,
+        'h-20': !aboveThreshold && !menuOpen,
+        'h-screen': aboveThreshold || menuOpen
       })}
     >
       <div className="flex items-center justify-between mb-10">
         <h1 className="font-bold text-2xl text-gray-700">
           {chapter ? (
             <>
-              {capAbbrev} {chapter?.chapter.number}
+              {capAbbrev === 'Job' ? 'Jó' : capAbbrev} {chapter?.chapter.number}
             </>
           ) : (
-            "Bíblia Sagrada"
+            'Bíblia Sagrada'
           )}
         </h1>
 
